2a9ac980edcfbbf8f7051a7c27e02d94f0485202
ElAm1ne <amine.el-hassani@dauphine.eu>
2022-12-10 20:22:17 +0100

    first commit

---
+++ src/main/java/com/example/ProjectStock/ProjectStockApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectStock/ProjectStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
