43e962cb01c29764e4ae4ab39a705c794c8a3a6c
Amine EL HASSANI <114306633+ElAm1ne@users.noreply.github.com>
2023-02-02 22:56:57 +0100

    Delete frontend directory

--- frontend/src/app/stock-search-history.spec.ts
+++
@@ -1,7 +0,0 @@
-import { StockSearchHistory } from './stock-search-history';
-
-describe('StockSearchHistory', () => {
-  it('should create an instance', () => {
-    expect(new StockSearchHistory()).toBeTruthy();
-  });
-});
